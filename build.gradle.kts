0fb8d349b6bee458aa0a1eba2a755d77386bbc14
OptiJava <OptiJava@protonmail.com>
2026-02-04 22:11:35 +0800

    fix(gradle): java toolchain error

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,6 @@ description = "loganyra-core"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
-        repositories {
-            // 修复windows上无法识别java工具链的bug
-            listOf("https://repo.gradle.org/gradle/java-toolchains/")
-        }
     }
 }
 
